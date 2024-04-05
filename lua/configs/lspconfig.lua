a56a7429f9ad6f12f552846b9082b369b24eb104
pogtato <redgerxu@gmail.com>
2024-04-04 22:16:20 -0700

    Initial commit

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,6 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
+local util = require "lspconfig/util"
 local servers = { "html", "cssls" }
 
 -- lsps with default config
@@ -20,4 +21,13 @@ lspconfig.tsserver.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
+}
+
+-- golang
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {"gopls"},
+  filetypes = {"go", "gomod", "gowork", "gotmpl"},
+  root_dir = util.root_pattern("go.mod")
 }
